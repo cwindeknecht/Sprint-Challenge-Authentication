98d8fdbf632e6579b357ca2648c9a9676c07ac1e
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-23 11:44:58 -0400

    createUser Functioning
    controllers/user.js --- createUser function implemented, checked with Postman/Compass.
    
    api/routes/routes.js --- Readme said the route was api/user, it was api/users so I changed it to api/user.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,8 +3,17 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
+  username.toLowerCase();
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const newUser = new User({username,password});
+  newUser.save((error, user) => {
+    if (error) return res.json({Error:error});
+    res.json({
+      success: "New User Created",
+      UserCreated: newUser,
+    })
+  })
 };
 
 module.exports = {
